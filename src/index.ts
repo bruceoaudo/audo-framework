5e47ebd6b70b7fe359dfaa455e9433e34ac91dd7
bruceoaudo <bruceodoyoaudo@gmail.com>
2025-07-04 16:31:24 +0300

    Add Main server class and Router class

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { audo, Options } from "./audo";
+
+//=====================================================//
+// Exported modules (Both ES6 and CommonJS modules)
+//=====================================================//
+export { audo, Options };
+
+module.exports = { audo };
---
+++ src/routes.ts
@@ -0,0 +1,76 @@
+import * as http from "node:http";
+
+// Example usage
+// const router = Router()
+// router.post("/", (req, res)=>{})
+
+interface Routes {
+  post: Map<
+    string,
+    (req: http.IncomingMessage, res: http.ServerResponse) => void
+  >;
+  get: Map<
+    string,
+    (req: http.IncomingMessage, res: http.ServerResponse) => void
+  >;
+  put: Map<
+    string,
+    (req: http.IncomingMessage, res: http.ServerResponse) => void
+  >;
+  update: Map<
+    string,
+    (req: http.IncomingMessage, res: http.ServerResponse) => void
+  >;
+  delete: Map<
+    string,
+    (req: http.IncomingMessage, res: http.ServerResponse) => void
+  >;
+}
+
+export class Router {
+  private routes: Routes;
+  constructor() {
+    this.routes = {
+      post: new Map(),
+      get: new Map(),
+      put: new Map(),
+      update: new Map(),
+      delete: new Map(),
+    };
+  }
+
+  get(
+    path: string,
+    callback: (req: http.IncomingMessage, res: http.ServerResponse) => void
+  ) {
+    this.routes.get.set(path, callback);
+  }
+
+  post(
+    path: string,
+    callback: (req: http.IncomingMessage, res: http.ServerResponse) => void
+  ) {
+    this.routes.post.set(path, callback);
+  }
+
+  put(
+    path: string,
+    callback: (req: http.IncomingMessage, res: http.ServerResponse) => void
+  ) {
+    this.routes.put.set(path, callback);
+  }
+
+  update(
+    path: string,
+    callback: (req: http.IncomingMessage, res: http.ServerResponse) => void
+  ) {
+    this.routes.update.set(path, callback);
+  }
+
+  delete(
+    path: string,
+    callback: (req: http.IncomingMessage, res: http.ServerResponse) => void
+  ) {
+    this.routes.delete.set(path, callback);
+  }
+}
